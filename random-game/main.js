51c109b4833c69af7515a6cadc9aeed5f6aba61e
Ksenia Gorina <fffound27@gmail.com>
2024-10-04 18:32:08 +0200

    feat: add board animation styles

--- random-game/main.js
+++ random-game/main.js
@@ -1,2 +1,6 @@
-import '/public/assets/styles/main.css'
+import "/public/assets/styles/main.css";
+
+document.querySelector(".board__tile").addEventListener("click", () => {
+  document.querySelector(".tile__cap").classList.toggle("lifted");
+});
 
